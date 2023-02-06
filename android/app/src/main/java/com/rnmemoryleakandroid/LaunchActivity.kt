b5d14adc4a05a636067222a45a7cc5f235a34a73
yinfei <yinfei@yinfei-MacBook-Pro.local>
2023-02-06 14:41:53 +0800

    init project

---
+++ android/app/src/main/java/com/rnmemoryleakandroid/LaunchActivity.kt
@@ -0,0 +1,36 @@
+package com.rnmemoryleakandroid
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.rnmemoryleakandroid.databinding.ActivityLaunchBinding
+
+class LaunchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLaunchBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLaunchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        binding.content.openRN.setOnClickListener {
+            startActivity(Intent(LaunchActivity@ this, MainActivity::class.java))
+        }
+    }
+
+}
